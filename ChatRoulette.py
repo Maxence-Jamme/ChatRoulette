14ea339f714501fe3a1e459334a497f7d199ecd3
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2024-10-04 09:43:45 +0200

    test auto pull 4

--- ChatRoulette.py
+++ ChatRoulette.py
@@ -268,7 +268,7 @@ async def ping(ctx):
 async def test(ctx):
     print("Commande !test reçue")
     print(ctx)
-    await ctx.send(f"test")
+    await ctx.send(f"test test")
 
 # Commande !test avec deux arguments (arg2 peut contenir des espaces)
 @bot.command(name="test1", hidden=True)
